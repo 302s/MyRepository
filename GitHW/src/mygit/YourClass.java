609b1c913a287d707772cd8be9a27725b67399e7
82108 <82108@DESKTOP-3J7CF3N>
2025-12-03 14:22:48 +0900

    Change in master branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -2,8 +2,9 @@ package mygit;
 
 public class YourClass {
 
-	public void sayHello(String toWhom) {
+	public static void main(String toWhom) {
 		System.out.println("Hello! " + toWhom);
+		System.out.println("Your change in master branch");
 		
 	}
 
